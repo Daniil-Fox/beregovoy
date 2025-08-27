c3fcb2f45aa63b1ba555e2b4b506797492e323de
Artyushenko Daniil <foxones80@gmail.com>
2025-08-27 13:38:59 +0600

    added elems

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,6 +1,7 @@
 import "./components/ex.js";
 import "./components/slider.js";
 import "./components/inputs.js";
+import "./components/fancy.js";
 import FAQ from "./components/faq.js";
 
 document.addEventListener("DOMContentLoaded", (e) => {
